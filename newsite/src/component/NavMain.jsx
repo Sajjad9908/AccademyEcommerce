0a250a4d2e0c76e51591b0259a22e86b42af4e5c
Sajjad9908 <gulbadan128@gmail.com>
2025-04-30 13:28:35 +0500

    SomeFooter Changes

--- newsite/src/component/NavMain.jsx
+++ newsite/src/component/NavMain.jsx
@@ -7,45 +7,48 @@ import { Link } from 'react-router-dom';
 const NavMain = () => {
   return (
     <>
-     <div className='nav'>
-        <div className='rounded-nav navreveal'>
-            <div className='nav-logo'>
+    <div className='nav'>
+               <div className='rounded-nav navreveal'>
+                   <div className='nav-logo'>
+              
+                    <img src='	https://themewagon.github.io/Oinia/images/logo.png'/>
+                   </div>
+                   <div className='links'>
+                       <input type='checkbox' id='toggle-btn' className='hidden'/>
+                       <label htmlFor='toggle-btn' className='menu-btn'>
+                       <CiMenuBurger size={22} fontWeight={200} />
+                       </label>
+                       <ul>
+                           <li className='li-nav'>
+                               <Link to={'/'}>
+                               <button className='links-btn active'>Home</button>
+                               </Link>
+                               <Link to={'/about'}>
+                               <button className='links-btn'>About</button>
+                               </Link>
+                               <Link to={'/services'}>
+                               <button className='links-btn'>Services</button>
+                               </Link>
+                               <Link to={'/contactus'}>
+                               <button className='links-btn'>Contact Us</button>
+   
+                               <button className=' nav-btn'>Logout</button>
+                               <IoSearch className='search-logout'/>
+                               </Link>
+                           </li>
+                       </ul>
+                   </div>
+                  
+                    
+                          
+                      
+                 
+                   
+                  
+                   
        
-             <img src='	https://themewagon.github.io/Oinia/images/logo.png'/>
-            </div>
-            <div className='links'>
-                <input type='checkbox' id='toggle-btn' className='hidden'/>
-                <label htmlFor='toggle-btn' className='menu-btn'>
-                <CiMenuBurger size={22} fontWeight={200} />
-                </label>
-                <ul>
-                    <li>
-                        <Link to={'/'}>
-                        <button className='links-btn active'>Home</button>
-                        </Link>
-                        <Link to={'/about'}>
-                        <button className='links-btn'>About</button>
-                        </Link>
-                        <Link to={'/services'}>
-                        <button className='links-btn'>Services</button>
-                        </Link>
-                        <Link to={'/contactus'}>
-                        <button className='links-btn'>Contact Us</button>
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-            <div className='logout-link'>
-                <li>
-                    <button className=' nav-btn'>Logout</button>
-                </li>
-            </div>
-            <div className='search-icon'>
-            <IoSearch />
-            </div>
-
-        </div>
-        </div>
+               </div>
+               </div>
         </>
   )
 }
